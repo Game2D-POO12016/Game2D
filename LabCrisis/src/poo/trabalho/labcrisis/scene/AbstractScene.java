bc4f5462c01839d6f062e16fc579573ae0b747f2
mickaelcosta <mickael.angelo.18@gmail.com>
2016-05-20 20:04:40 -0300

    Add files via upload

--- LabCrisis/src/poo/trabalho/labcrisis/scene/AbstractScene.java
+++ LabCrisis/src/poo/trabalho/labcrisis/scene/AbstractScene.java
@@ -5,21 +5,19 @@ import org.andengine.engine.camera.Camera;
 import org.andengine.entity.scene.Scene;
 import org.andengine.opengl.vbo.VertexBufferObjectManager;
 import org.andengine.util.debug.Debug;
-
 import poo.trabalho.labcrisis.GameActivity;
 import poo.trabalho.labcrisis.ResourceManager;
 
-public abstract class AbstractScene extends Scene {
-	
-protected ResourceManager res = ResourceManager.getInstance();
-protected Engine engine = res.engine;
-protected GameActivity activity = res.activity;
-protected VertexBufferObjectManager vbom = res.vbom;
-protected Camera camera = res.camera;
-
+public abstract class AbstractScene extends Scene {	
+	protected ResourceManager res = ResourceManager.getInstance();
+	protected Engine engine = res.engine;
+	protected GameActivity activity = res.activity;
+	protected VertexBufferObjectManager vbom = res.vbom;
+	protected Camera camera = res.camera;
 	public abstract void populate();
 	
-	public void destroy() {}
+	public void destroy() {
+	}
 
 	public void onBackKeyPressed() {
 		Debug.d("Back key pressed");
--- LabCrisis/src/poo/trabalho/labcrisis/scene/SceneManager.java
+++ LabCrisis/src/poo/trabalho/labcrisis/scene/SceneManager.java
@@ -1,101 +1,100 @@
 package poo.trabalho.labcrisis.scene;
 
 import org.andengine.util.debug.Debug;
-
 import android.os.AsyncTask;
 import poo.trabalho.labcrisis.ResourceManager;
 
 public class SceneManager {
-	// single instance is created only
 	private static final SceneManager INSTANCE = new SceneManager();
 	public static final long SPLASH_DURATION = 2000;
 	private ResourceManager res = ResourceManager.getInstance();
 	private AbstractScene currentScene;
 	private LoadingScene loadingScene = null;
-	private SceneManager() { }
-	public static SceneManager getInstance() {
-	return INSTANCE;
-	}
-	public AbstractScene getCurrentScene() {
-	return currentScene;
-	}
-	public void setCurrentScene(AbstractScene currentScene) {
-	this.currentScene = currentScene;
-	res.engine.setScene(currentScene);
-	Debug.i("Current scene: " + currentScene.getClass().
-	getName());
+	
+	private SceneManager() {
 	}
 	
+	public static SceneManager getInstance() {
+		return INSTANCE;
+	}
+	
+	public AbstractScene getCurrentScene() {
+		return currentScene;
+	}
+	
+	public void setCurrentScene(AbstractScene currentScene) {
+		this.currentScene = currentScene;
+		res.engine.setScene(currentScene);
+		Debug.i("Current scene: " + currentScene.getClass().getName());
+	}
 	
 	public AbstractScene showSplashAndMenuScene() {
 		final SplashScene splashScene = new SplashScene();
 		splashScene.populate();
 		setCurrentScene(splashScene);
 		new AsyncTask<Void, Void, Void>() {
-		@Override
-		protected Void doInBackground(Void... params) {
-		long timestamp = System.currentTimeMillis();
-		res.loadFont();
-		res.loadGameAudio();
-		res.loadGameGraphics();
-		loadingScene = new LoadingScene();
-		loadingScene.populate();
-		AbstractScene nextScene = new MenuSceneWrapper();
-		if (System.currentTimeMillis() - timestamp <
-		SPLASH_DURATION) {
-		try {
-		Thread.sleep(SPLASH_DURATION -
-		(System.currentTimeMillis() - timestamp) );
-		} catch (InterruptedException e) {
-		Debug.e("Interrupted", e);
-		}
-		}
-		nextScene.populate();
-		setCurrentScene(nextScene);
-		splashScene.destroy();
-		res.unloadSplashGraphics();
-		return null;
-		}
+			@Override
+			protected Void doInBackground(Void... params) {
+				long timestamp = System.currentTimeMillis();
+				res.loadFont();
+				res.loadGameAudio();
+				res.loadGameGraphics();
+				loadingScene = new LoadingScene();
+				loadingScene.populate();
+				AbstractScene nextScene = new MenuSceneWrapper();
+				
+				if (System.currentTimeMillis() - timestamp < SPLASH_DURATION) {
+					try {
+						Thread.sleep(SPLASH_DURATION - (System.currentTimeMillis() - timestamp) );
+					} catch (InterruptedException e) {
+						Debug.e("Interrupted", e);
+					}
+				}
+				
+				nextScene.populate();
+				setCurrentScene(nextScene);
+				splashScene.destroy();
+				res.unloadSplashGraphics();
+				return null;
+			}
 		}.execute();
 		return splashScene;
-		}
+	}
 	
 	public void showGameScene() {
 		final AbstractScene previousScene = getCurrentScene();
 		setCurrentScene(loadingScene);
 		new AsyncTask<Void, Void, Void>() {
-		@Override
-		protected Void doInBackground(Void... params) {
-		try {
-		Thread.sleep(1000);
-		} catch (InterruptedException e) {
-		Debug.e("Interrupted", e);
-		}
-		Fase_01Scene gameScene = new Fase_01Scene();
-		gameScene.populate();
-		previousScene.destroy();
-		setCurrentScene(gameScene);
-		return null;
-		}
+			@Override
+			protected Void doInBackground(Void... params) {
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					Debug.e("Interrupted", e);
+				}
+		
+				Fase_01Scene gameScene = new Fase_01Scene();
+				gameScene.populate();
+				previousScene.destroy();
+				setCurrentScene(gameScene);
+				return null;
+			}
 		}.execute();
-		}
+	}
 	
 	public void showMenuScene() {
 		final AbstractScene previousScene = getCurrentScene();
 		setCurrentScene(loadingScene);
-		new AsyncTask<Void, Void, Void>() {
-		@Override
-		protected Void doInBackground(Void... params) {
-		MenuSceneWrapper menuSceneWrapper = new MenuSceneWrapper();
-		menuSceneWrapper.populate();
-		setCurrentScene(menuSceneWrapper);
-		previousScene.destroy();
-		return null;
-		}
-		}.execute();
-		}
-	
-	
-	
-	
+			new AsyncTask<Void, Void, Void>() {
+				@Override
+				protected Void doInBackground(Void... params) {
+					MenuSceneWrapper menuSceneWrapper = new MenuSceneWrapper();
+					menuSceneWrapper.populate();
+					setCurrentScene(menuSceneWrapper);
+					previousScene.destroy();
+					return null;
+				}
+			}.execute();
 	}
+	
+}
