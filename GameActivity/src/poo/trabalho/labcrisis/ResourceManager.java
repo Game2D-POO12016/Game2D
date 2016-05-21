36fca3c6e6d2e51e72e8126458d1cad2df19419b
Victor <victoraugusto16@gmail.com>
2016-05-21 11:56:35 -0300

    Removida a dependência do app_compat7
    Por favor, testar direto no celular.

---
+++ GameActivity/src/poo/trabalho/labcrisis/ResourceManager.java
@@ -0,0 +1,122 @@
+package poo.trabalho.labcrisis;
+
+import org.andengine.audio.music.Music;
+import org.andengine.audio.music.MusicFactory;
+import org.andengine.audio.sound.Sound;
+import org.andengine.audio.sound.SoundFactory;
+import org.andengine.engine.Engine;
+import org.andengine.engine.camera.Camera;
+import org.andengine.opengl.font.Font;
+import org.andengine.opengl.font.FontFactory;
+import org.andengine.opengl.texture.TextureOptions;
+import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlas;
+import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlasTextureRegionFactory;
+import org.andengine.opengl.texture.atlas.bitmap.BuildableBitmapTextureAtlas;
+import org.andengine.opengl.texture.atlas.bitmap.source.IBitmapTextureAtlasSource;
+import org.andengine.opengl.texture.atlas.buildable.builder.BlackPawnTextureAtlasBuilder;
+import org.andengine.opengl.texture.atlas.buildable.builder.ITextureAtlasBuilder.TextureAtlasBuilderException;
+import org.andengine.opengl.texture.bitmap.BitmapTextureFormat;
+import org.andengine.opengl.texture.region.ITextureRegion;
+import org.andengine.opengl.texture.region.ITiledTextureRegion;
+import org.andengine.opengl.vbo.VertexBufferObjectManager;
+import org.andengine.util.adt.color.Color;
+import android.graphics.Typeface;
+
+public class ResourceManager {
+	public GameActivity activity;
+	public Engine engine;
+	public Camera camera;
+	public VertexBufferObjectManager vbom;
+	public ITextureRegion splashTextureRegion;
+	private BitmapTextureAtlas splashTextureAtlas;
+	
+	public void loadSplashGraphics() {
+		BitmapTextureAtlasTextureRegionFactory.setAssetBasePath("gfx/");
+		splashTextureAtlas = new BitmapTextureAtlas(activity.getTextureManager(), 256, 256,BitmapTextureFormat.RGBA_8888,TextureOptions.BILINEAR_PREMULTIPLYALPHA);
+		splashTextureRegion = BitmapTextureAtlasTextureRegionFactory.createFromAsset(splashTextureAtlas, activity.getAssets(),"badge.png", 0, 0);
+		splashTextureAtlas.load();
+	}
+	
+	public void unloadSplashGraphics() {
+		splashTextureAtlas.unload();
+	}
+	
+	private static final ResourceManager INSTANCE = new ResourceManager();
+	
+	private ResourceManager() { }
+		public static ResourceManager getInstance() {
+			return INSTANCE;
+	}
+	
+   //cria o resource manager
+	public void create(GameActivity activity, Engine engine, Camera
+			camera, VertexBufferObjectManager vbom) {
+			this.activity = activity;
+			this.engine = engine;
+			this.camera = camera;
+			this.vbom = vbom;
+			}
+	
+	//game textures
+	public ITiledTextureRegion paredeTextureRegion;
+	public ITextureRegion comidaTextureRegion;
+	public ITiledTextureRegion globuloTextureRegion;
+	private BuildableBitmapTextureAtlas gameTextureAtlas;
+	
+	public void loadGameGraphics() {
+		BitmapTextureAtlasTextureRegionFactory.setAssetBasePath("gfx/");
+		gameTextureAtlas = new BuildableBitmapTextureAtlas(activity.getTextureManager(),1024, 512, BitmapTextureFormat.RGBA_8888, TextureOptions.BILINEAR_PREMULTIPLYALPHA);
+		
+		paredeTextureRegion = BitmapTextureAtlasTextureRegionFactory.
+		createTiledFromAsset(
+		gameTextureAtlas, activity.getAssets(), "Parede.png", 4, 1);
+		
+		globuloTextureRegion = BitmapTextureAtlasTextureRegionFactory.
+		createTiledFromAsset(
+		gameTextureAtlas, activity.getAssets(), "Globulo.png", 1, 1);
+		
+		comidaTextureRegion = BitmapTextureAtlasTextureRegionFactory.
+		createFromAsset(
+		gameTextureAtlas, activity.getAssets(), "comida.png");
+		
+		try {
+			gameTextureAtlas.build(new
+			BlackPawnTextureAtlasBuilder<IBitmapTextureAtlasSource,
+			BitmapTextureAtlas>(2, 0, 2));
+			gameTextureAtlas.load();		
+		} catch (final TextureAtlasBuilderException e) {
+			throw new RuntimeException("Error while loading game textures", e);
+		}
+	}
+	
+	
+	//sounds
+	public Sound soundGameover;
+	public Sound soundComer;
+	
+	//musics
+	public Music music;
+	
+	public void loadGameAudio() {
+		try {
+			SoundFactory.setAssetBasePath("sfx/");
+			soundComer = SoundFactory.createSoundFromAsset(activity.getSoundManager(), activity, "comer.wav");
+			soundGameover = SoundFactory.createSoundFromAsset(activity.getSoundManager(), activity, "gameOver.wav");
+			MusicFactory.setAssetBasePath("mfx/");
+			music = MusicFactory.createMusicFromAsset(activity.getMusicManager(), activity, "music.wav");
+		} catch (Exception e) {
+		throw new RuntimeException("Error while loading audio", e);
+		}
+	}
+	
+	//fonte
+	public Font font;
+	
+	public void loadFont() {
+		font = FontFactory.createStroke(activity.getFontManager(),activity.getTextureManager(), 256, 256,
+		Typeface.create(Typeface.SANS_SERIF, Typeface.BOLD), 50,true, Color.WHITE_ABGR_PACKED_INT, 2,Color.BLACK_ABGR_PACKED_INT);
+		font.prepareLetters("01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ.,!?".toCharArray());
+		font.load();
+	}
+		
+}
---
+++ GameActivity/src/poo/trabalho/labcrisis/scene/MenuSceneWrapper.java
@@ -0,0 +1,51 @@
+package poo.trabalho.labcrisis.scene;
+
+import org.andengine.entity.scene.menu.MenuScene;
+import org.andengine.entity.scene.menu.item.decorator.*;
+import org.andengine.entity.scene.menu.MenuScene.IOnMenuItemClickListener;
+import org.andengine.entity.scene.menu.item.IMenuItem;
+import org.andengine.entity.scene.menu.item.TextMenuItem;
+import org.andengine.entity.sprite.Sprite;
+import org.andengine.util.adt.color.Color;
+
+public class MenuSceneWrapper extends AbstractScene implements IOnMenuItemClickListener {
+	private IMenuItem playMenuItem;
+	
+	@Override
+	public void populate() {
+		MenuScene menuScene = new MenuScene(camera);
+		menuScene.getBackground().setColor(0.82f, 0.96f, 0.97f);
+		playMenuItem = new ColorMenuItemDecorator(new TextMenuItem(0,res.font, "PLAY", vbom), Color.CYAN, Color.WHITE);
+		menuScene.addMenuItem(playMenuItem);
+		menuScene.buildAnimations();
+		menuScene.setBackgroundEnabled(true);
+		menuScene.setOnMenuItemClickListener(this);
+		Sprite player = new Sprite(240, 640, res.globuloTextureRegion,vbom);
+		menuScene.attachChild(player);
+		setChildScene(menuScene);
+	}
+	
+	@Override
+	public void onPause() {
+	}
+	@Override
+	public void onResume() {
+	}
+	
+	@Override
+	public boolean onMenuItemClicked (MenuScene pMenuScene, IMenuItem pMenuItem, float pMenuItemLocalX, float pMenuItemLocalY) {
+		switch (pMenuItem.getID()) {
+			case 0 :
+				SceneManager.getInstance().showGameScene();
+				return true;
+			default :
+				return false;
+		}
+	}
+	
+	@Override
+	public void onBackKeyPressed() {
+		activity.finish();
+	}
+	
+}
