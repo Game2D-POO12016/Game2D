2bd21101ee82b19fd2e35d13ae713c75aa317cc7
mickaelcosta <mickael.angelo.18@gmail.com>
2016-05-22 03:25:11 -0300

    Add files via upload

--- LabCrisis/src/poo/trabalho/labcrisis/entity/Comida.java
+++ LabCrisis/src/poo/trabalho/labcrisis/entity/Comida.java
@@ -1,5 +1,12 @@
 package poo.trabalho.labcrisis.entity;
 
-public class Comida {
+import org.andengine.entity.sprite.TiledSprite;
+import org.andengine.opengl.texture.region.ITiledTextureRegion;
+import org.andengine.opengl.vbo.VertexBufferObjectManager;
 
-}
+	public class Comida extends TiledSprite {
+		public Comida(float pX, float pY,ITiledTextureRegion pTiledTextureRegion,VertexBufferObjectManager pVertexBufferObjectManager) {
+			super(pX, pY, pTiledTextureRegion,pVertexBufferObjectManager);
+		}
+
+	}
