fdbfc5f190252f3a29b3a3739461d5939e3ed3ff
mickaelcosta <mickael.angelo.18@gmail.com>
2016-05-22 03:26:17 -0300

    versao mickael

--- LabCrisis/src/poo/trabalho/labcrisis/scene/Fase_01Scene.java
+++ LabCrisis/src/poo/trabalho/labcrisis/scene/Fase_01Scene.java
@@ -1,5 +1,6 @@
 package poo.trabalho.labcrisis.scene;
 
+import org.andengine.engine.camera.hud.HUD;
 import org.andengine.engine.handler.collision.CollisionHandler;
 import org.andengine.engine.handler.collision.ICollisionCallback;
 import org.andengine.entity.Entity;
@@ -8,111 +9,169 @@ import org.andengine.entity.scene.IOnSceneTouchListener;
 import org.andengine.entity.scene.Scene;
 import org.andengine.entity.scene.background.EntityBackground;
 import org.andengine.entity.shape.IShape;
-import org.andengine.entity.sprite.Sprite;
 import org.andengine.entity.text.Text;
 import org.andengine.input.touch.TouchEvent;
 import org.andengine.util.adt.align.HorizontalAlign;
 import org.andengine.util.adt.color.Color;
 import org.andengine.entity.text.*;
-
+import poo.trabalho.labcrisis.MusicPlayer;
+import poo.trabalho.labcrisis.ResourceManager;
+import poo.trabalho.labcrisis.entity.Comida;
 import poo.trabalho.labcrisis.entity.Parede;
 import poo.trabalho.labcrisis.entity.Player;
+import poo.trabalho.labcrisis.factory.ComidaFactory;
 import poo.trabalho.labcrisis.factory.ParedeFactory;
 import poo.trabalho.labcrisis.factory.PlayerFactory;
 
 public class Fase_01Scene extends AbstractScene {
-	
 	private Parede parede;
+	private Comida comida;
 	private Text scoreText;
 	private Player player;
 	
-	
 	public Fase_01Scene() {
 		ParedeFactory.getInstance().create(vbom);
+		ComidaFactory.getInstance().create(vbom);
 		PlayerFactory.getInstance().create(vbom);
-		}
+	}
 	
 	@Override
 	public void populate() {
-	createBackground();
-	createParede();
-	createPlayer();
-	
-	//escrevendo textos
-	scoreText = new Text(16, 784, res.font, "SCORE X", new
-			TextOptions(HorizontalAlign.LEFT), vbom);
-			scoreText.setAnchorCenter(0, 1);
-			attachChild(scoreText);
-			
-			
-			setOnSceneTouchListener(new IOnSceneTouchListener() {
-				@Override
-				public boolean onSceneTouchEvent(Scene pScene, TouchEvent
-				pSceneTouchEvent) {
-				if (pSceneTouchEvent.isActionDown()) {
+		createBackground();
+		createParede();
+		createComida(150,300);
+		createComida(200,300);
+		createComida(250,300);
+		createComida(300,300);
+		createComida(350,300);
+		createPlayer();
+		createHUD();
+
+	setOnSceneTouchListener(new IOnSceneTouchListener() {
+		@Override
+		public boolean onSceneTouchEvent(Scene pScene, TouchEvent pSceneTouchEvent) {
+			if (pSceneTouchEvent.isActionDown()) {
 				player.clearEntityModifiers();
-				player.registerEntityModifier(new MoveModifier(1,
-						player.getX(), player.getY(), pSceneTouchEvent.getX(),
-						pSceneTouchEvent.getY()));
-						return true;
-						}
-						return false;
-						}
-						});
+				player.registerEntityModifier(new MoveModifier(1,player.getX(), player.getY(), pSceneTouchEvent.getX(),pSceneTouchEvent.getY()));
+				ResourceManager.getInstance().soundComer.play();
+				return true;
+			}
+			return false;
+		}
+	});
 			
-			registerTouchArea(player);
-			
-			
-			ICollisionCallback myCollisionCallback = new ICollisionCallback
-					() {
-					@Override
-					public boolean onCollision(IShape pCheckShape, IShape
-					pTargetShape) {
-					parede.setColor(Color.RED);
-					return false;
-					}
-					};
-					CollisionHandler myCollisionHandler = new
-					CollisionHandler(myCollisionCallback, parede, player);
-					registerUpdateHandler(myCollisionHandler);
-			
-			
+	registerTouchArea(player);
+	
+	ICollisionCallback myCollisionCallback = new ICollisionCallback() {
+		@Override
+		public boolean onCollision(IShape pCheckShape, IShape pTargetShape) {
+			parede.setColor(Color.RED);
+			ResourceManager.getInstance().soundGameover.play();
+			return false;
+		}
+	};
+	
+	CollisionHandler myCollisionHandler = new CollisionHandler(myCollisionCallback, parede, player);
+	registerUpdateHandler(myCollisionHandler);
+					
+	MusicPlayer.getInstance().play();		
+	camera.setChaseEntity(player);
 	}
 	
 	@Override
 	public void onBackKeyPressed() {
-	SceneManager.getInstance().showMenuScene();
+		SceneManager.getInstance().showMenuScene();
+		MusicPlayer.getInstance().stop();
 	}
 	
 	private void createParede() {
-		parede = ParedeFactory.getInstance().createParede(240, 400);
-		attachChild(parede);
+		
+		//paredes superiores
+		for(int i = 100 ; i<= 440 ; i = i+ 20){
+			parede = ParedeFactory.getInstance().createParede(i, 400);
+			parede.setCurrentTileIndex(4);
+			parede.setScale((float) 0.2);
+			attachChild(parede);
 		}
-
-	
-	private void createBackground() {
-	Entity background = new Entity();
-	//Sprite comidaSprite = new Sprite(200, 300, res.comidaTextureRegion,
-	//vbom);
-	//background.attachChild(comidaSprite);
-	setBackground(new EntityBackground(0.82f, 0.96f, 0.97f,
-	background));
+		
+		
+		//paredes inferiores
+		for(int i = 10 ; i<= 340 ; i = i+ 20){
+			parede = ParedeFactory.getInstance().createParede(i, 200);
+			parede.setCurrentTileIndex(4);
+			parede.setScale((float) 0.2);
+			attachChild(parede);
+		}
+		
+		
+		//lateral esquerda
+		for(int i = 800 ; i>= 200 ; i = i- 20){
+			parede = ParedeFactory.getInstance().createParede(10, i);
+			parede.setCurrentTileIndex(4);
+			parede.setScale((float) 0.2);
+			attachChild(parede);
+		}
+		
+		//lateral direita
+		for(int i = 800 ; i>= 400 ; i = i- 20){
+			parede = ParedeFactory.getInstance().createParede(100, i);
+			parede.setCurrentTileIndex(4);
+			parede.setScale((float) 0.2);
+			attachChild(parede);
+		}
+		
+		//segunda lateral direita
+		for(int i = 400 ; i>= 20 ; i = i- 20){
+			parede = ParedeFactory.getInstance().createParede(440, i);
+			parede.setCurrentTileIndex(4);
+			parede.setScale((float) 0.2);
+			attachChild(parede);
+		}	
+				
+		//segunda lateral esquerda
+		for(int i = 200 ; i>= 20 ; i = i- 20){
+			parede = ParedeFactory.getInstance().createParede(340, i);
+			parede.setCurrentTileIndex(4);
+			parede.setScale((float) 0.2);
+			attachChild(parede);
+		}
+				
 	}
 	
+	private void createComida(int x, int y) {
+		comida = ComidaFactory.getInstance().createComida(x, y);
+		comida.setScale((float) 0.5);
+		attachChild(comida);
+	}
+
+	private void createBackground() {
+		Entity background = new Entity();
+		setBackground(new EntityBackground(0.82f, 0.96f, 0.97f, background));
+	}
 	
 	private void createPlayer() {
-		player = PlayerFactory.getInstance().createPlayer(240, 600);
+		player = PlayerFactory.getInstance().createPlayer(50, 600);
 		player.setScale((float) 0.2);
 		attachChild(player);
-		}
+	}
 	
-	
+	private void createHUD() {
+		HUD hud = new HUD();
+		//escrevendo textos
+		scoreText = new Text(16, 784, res.font, "SCORE X", new TextOptions(HorizontalAlign.LEFT), vbom);
+		scoreText.setAnchorCenter(0, 1);						
+		hud.attachChild(scoreText);
+		camera.setHUD(hud);
+	}
 	
 	@Override
 	public void onPause() {
+		MusicPlayer.getInstance().pause();
 	}
 	
 	@Override
 	public void onResume() {
+		MusicPlayer.getInstance().play();
 	}
-	}
+	
+}
--- LabCrisis/src/poo/trabalho/labcrisis/scene/MenuSceneWrapper.java
+++ LabCrisis/src/poo/trabalho/labcrisis/scene/MenuSceneWrapper.java
@@ -3,30 +3,26 @@ package poo.trabalho.labcrisis.scene;
 import org.andengine.entity.scene.menu.MenuScene;
 import org.andengine.entity.scene.menu.item.decorator.*;
 import org.andengine.entity.scene.menu.MenuScene.IOnMenuItemClickListener;
-import org.andengine.engine.camera.Camera;
 import org.andengine.entity.scene.menu.item.IMenuItem;
 import org.andengine.entity.scene.menu.item.TextMenuItem;
 import org.andengine.entity.sprite.Sprite;
 import org.andengine.util.adt.color.Color;
 
 public class MenuSceneWrapper extends AbstractScene implements IOnMenuItemClickListener {
-
 	private IMenuItem playMenuItem;
 	
 	@Override
 	public void populate() {
-	MenuScene menuScene = new MenuScene(camera);
-	menuScene.getBackground().setColor(0.82f, 0.96f, 0.97f);
-	playMenuItem = new ColorMenuItemDecorator(new TextMenuItem(0,
-	res.font, "PLAY", vbom), Color.CYAN, Color.WHITE);
-	menuScene.addMenuItem(playMenuItem);
-	menuScene.buildAnimations();
-	menuScene.setBackgroundEnabled(true);
-	menuScene.setOnMenuItemClickListener(this);
-	Sprite player = new Sprite(240, 640, res.globuloTextureRegion,
-	vbom);
-	menuScene.attachChild(player);
-	setChildScene(menuScene);
+		MenuScene menuScene = new MenuScene(camera);
+		menuScene.getBackground().setColor(0.82f, 0.96f, 0.97f);
+		playMenuItem = new ColorMenuItemDecorator(new TextMenuItem(0,res.font, "PLAY", vbom), Color.CYAN, Color.WHITE);
+		menuScene.addMenuItem(playMenuItem);
+		menuScene.buildAnimations();
+		menuScene.setBackgroundEnabled(true);
+		menuScene.setOnMenuItemClickListener(this);
+		Sprite player = new Sprite(240, 640, res.globuloTextureRegion,vbom);
+		menuScene.attachChild(player);
+		setChildScene(menuScene);
 	}
 	
 	@Override
@@ -38,18 +34,18 @@ public class MenuSceneWrapper extends AbstractScene implements IOnMenuItemClickL
 	
 	@Override
 	public boolean onMenuItemClicked (MenuScene pMenuScene, IMenuItem pMenuItem, float pMenuItemLocalX, float pMenuItemLocalY) {
-	switch (pMenuItem.getID()) {
-	case 0 :
-	SceneManager.getInstance().showGameScene();
-	return true;
-	default :
-		return false;
-	}
-	}
-	@Override
-	public void onBackKeyPressed() {
-	activity.finish();
+		switch (pMenuItem.getID()) {
+			case 0 :
+				SceneManager.getInstance().showGameScene();
+				return true;
+			default :
+				return false;
+		}
 	}
 	
-
+	@Override
+	public void onBackKeyPressed() {
+		activity.finish();
+	}
+	
 }
