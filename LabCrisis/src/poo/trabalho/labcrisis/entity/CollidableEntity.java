4ef3574543832e5c6b92dc130ae02271e3e34da2
ivansena35 <ivansena35@gmail.com>
2016-05-23 01:00:59 -0300

    interface para a colisão de entidades

--- LabCrisis/src/poo/trabalho/labcrisis/entity/CollidableEntity.java
+++ LabCrisis/src/poo/trabalho/labcrisis/entity/CollidableEntity.java
@@ -1,5 +1,10 @@
 package poo.trabalho.labcrisis.entity;
 
-public interface CollidableEntity {
+import org.andengine.entity.IEntity;
+import com.badlogic.gdx.physics.box2d.Body;
 
+public interface CollidableEntity extends IEntity {
+	public void setBody(Body body);
+	public Body getBody();
+	public String getType();
 }
