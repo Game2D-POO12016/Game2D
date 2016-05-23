daeea1f91ac650bf0a0358342eb7b823ae20f738
ivansena35 <ivansena35@gmail.com>
2016-05-23 01:10:30 -0300

    um player ocupa um lugar no espaço

--- LabCrisis/src/poo/trabalho/labcrisis/factory/PlayerFactory.java
+++ LabCrisis/src/poo/trabalho/labcrisis/factory/PlayerFactory.java
@@ -2,27 +2,44 @@ package poo.trabalho.labcrisis.factory;
 
 import poo.trabalho.labcrisis.*;
 import poo.trabalho.labcrisis.entity.Player;
+
+import org.andengine.extension.physics.box2d.PhysicsConnector;
+import org.andengine.extension.physics.box2d.PhysicsFactory;
+import org.andengine.extension.physics.box2d.PhysicsWorld;
 import org.andengine.opengl.vbo.VertexBufferObjectManager;
 
-	public class PlayerFactory {
-		private static PlayerFactory INSTANCE = new PlayerFactory();
-		private VertexBufferObjectManager vbom;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
 
-		private PlayerFactory() {
-		}	
+public class PlayerFactory {
+	private static PlayerFactory INSTANCE = new PlayerFactory();
+	private VertexBufferObjectManager vbom;
+	private PhysicsWorld physicsWorld;
+	public static final FixtureDef PLAYER_FIXTURE = PhysicsFactory.createFixtureDef(1f, 0f, 1f, false);
 
-		public static PlayerFactory getInstance() {
-			return INSTANCE;
-		}
+	private PlayerFactory() {
+	}	
 
-		public void create(VertexBufferObjectManager vbom) {
-			this.vbom = vbom;
-		}
-
-		public Player createPlayer(float x, float y) {
-			Player player = new Player(x, y, ResourceManager.getInstance().globuloTextureRegion, vbom);
-			player.setZIndex(2);
-			return player;
-		}
-		
+	public static PlayerFactory getInstance() {
+		return INSTANCE;
 	}
+
+	public void create(PhysicsWorld physicsWorld,VertexBufferObjectManager vbom) {
+		this.physicsWorld = physicsWorld;
+		this.vbom = vbom;
+	}
+
+	public Player createPlayer(float x, float y) {
+		Player player = new Player(x, y, ResourceManager.getInstance().globuloTextureRegion, vbom);
+		player.setZIndex(2);
+		Body playerBody = PhysicsFactory.createBoxBody(physicsWorld,player, BodyType.DynamicBody, PLAYER_FIXTURE);
+		playerBody.setLinearDamping(1f);
+		playerBody.setFixedRotation(true);
+		playerBody.setUserData(player);
+		physicsWorld.registerPhysicsConnector(new PhysicsConnector(player, playerBody));
+		player.setBody(playerBody);
+		return player;
+	}
+	
+}
