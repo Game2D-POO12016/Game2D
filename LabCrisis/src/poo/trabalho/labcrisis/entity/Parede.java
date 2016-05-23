5b2e63aef1b739658345fc0a70f694b5d41926c0
ivansena35 <ivansena35@gmail.com>
2016-05-23 01:04:27 -0300

    parede implementa interface de colisão

--- LabCrisis/src/poo/trabalho/labcrisis/entity/Parede.java
+++ LabCrisis/src/poo/trabalho/labcrisis/entity/Parede.java
@@ -4,9 +4,34 @@ import org.andengine.entity.sprite.TiledSprite;
 import org.andengine.opengl.texture.region.ITiledTextureRegion;
 import org.andengine.opengl.vbo.VertexBufferObjectManager;
 
-	public class Parede extends TiledSprite {
-		public Parede(float pX, float pY,ITiledTextureRegion pTiledTextureRegion,VertexBufferObjectManager pVertexBufferObjectManager) {
-			super(pX, pY, pTiledTextureRegion,pVertexBufferObjectManager);
-		}
+import com.badlogic.gdx.physics.box2d.Body;
 
+public class Parede extends TiledSprite implements CollidableEntity{
+	private Body body;
+	public static final String TYPE = "Platform";
+	
+	public Parede(float pX, float pY,ITiledTextureRegion pTiledTextureRegion,VertexBufferObjectManager pVertexBufferObjectManager) {
+		super(pX, pY, pTiledTextureRegion,pVertexBufferObjectManager);
 	}
+	
+	@Override
+	protected void onManagedUpdate(float pSecondsElapsed) {
+		super.onManagedUpdate(pSecondsElapsed);
+	}
+	
+	@Override
+	public void setBody(Body body) {
+		this.body = body;
+	}
+	
+	@Override
+	public Body getBody() {
+		return body;
+	}
+	
+	@Override
+	public String getType() {
+		return TYPE;
+	}	
+		
+}
