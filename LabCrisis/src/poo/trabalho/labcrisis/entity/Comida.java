f1aebe420ed485efcaa882b37c2622eec01931b4
ivansena35 <ivansena35@gmail.com>
2016-05-23 01:03:17 -0300

    entidade comida implementa interface para colisão

--- LabCrisis/src/poo/trabalho/labcrisis/entity/Comida.java
+++ LabCrisis/src/poo/trabalho/labcrisis/entity/Comida.java
@@ -4,9 +4,32 @@ import org.andengine.entity.sprite.TiledSprite;
 import org.andengine.opengl.texture.region.ITiledTextureRegion;
 import org.andengine.opengl.vbo.VertexBufferObjectManager;
 
-	public class Comida extends TiledSprite {
-		public Comida(float pX, float pY,ITiledTextureRegion pTiledTextureRegion,VertexBufferObjectManager pVertexBufferObjectManager) {
-			super(pX, pY, pTiledTextureRegion,pVertexBufferObjectManager);
-		}
+import com.badlogic.gdx.physics.box2d.Body;
 
+public class Comida extends TiledSprite implements CollidableEntity{
+	private Body body;
+	public static final String TYPE = "Platform";
+	public Comida(float pX, float pY,ITiledTextureRegion pTiledTextureRegion,VertexBufferObjectManager pVertexBufferObjectManager) {
+		super(pX, pY, pTiledTextureRegion,pVertexBufferObjectManager);
 	}
+	
+	@Override
+	protected void onManagedUpdate(float pSecondsElapsed) {
+		super.onManagedUpdate(pSecondsElapsed);
+	}
+	
+	@Override
+	public void setBody(Body body) {
+		this.body = body;
+	}
+	
+	@Override
+	public Body getBody() {
+		return body;
+	}
+	
+	@Override
+	public String getType() {
+		return TYPE;
+	}	
+}
