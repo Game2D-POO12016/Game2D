f0b6ba62ccd2465a942bc19b759c24158002aed0
ivansena35 <ivansena35@gmail.com>
2016-05-23 01:05:57 -0300

    player implementa a interface de colisão

--- LabCrisis/src/poo/trabalho/labcrisis/entity/Player.java
+++ LabCrisis/src/poo/trabalho/labcrisis/entity/Player.java
@@ -4,8 +4,25 @@ import org.andengine.entity.sprite.TiledSprite;
 import org.andengine.opengl.texture.region.ITiledTextureRegion;
 import org.andengine.opengl.vbo.VertexBufferObjectManager;
 
-public class Player extends TiledSprite {
+import com.badlogic.gdx.physics.box2d.Body;
+
+public class Player extends TiledSprite implements CollidableEntity{
 	boolean dead = false;
+	private Body body;
+	public static final String TYPE = "Player";
+	
+	@Override
+	public void setBody(Body body) {
+		this.body = body;
+	}
+	@Override
+	public Body getBody() {
+		return body;
+	}
+	@Override
+	public String getType() {
+		return TYPE;
+	}
 	
 	public Player(float pX, float pY,ITiledTextureRegion pTiledTextureRegion,VertexBufferObjectManager pVertexBufferObjectManager) {
 		super(pX, pY, pTiledTextureRegion,pVertexBufferObjectManager);
