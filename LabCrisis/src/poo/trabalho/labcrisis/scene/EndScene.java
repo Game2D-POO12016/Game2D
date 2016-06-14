8d6390217c92ca5eb67680fb01819e77b9092408
Alexandre Correia <alexandre.correia.mesquita@gmail.com>
2016-06-14 13:45:48 -0300

    Finalizacao da EndScene.

--- LabCrisis/src/poo/trabalho/labcrisis/scene/EndScene.java
+++ LabCrisis/src/poo/trabalho/labcrisis/scene/EndScene.java
@@ -5,11 +5,14 @@ import org.andengine.entity.text.Text;
 import org.andengine.entity.text.TextOptions;
 import org.andengine.util.adt.align.HorizontalAlign;
 
+import poo.trabalho.labcrisis.GameManager;
 import poo.trabalho.labcrisis.MusicPlayer;
 
 public class EndScene extends AbstractScene {
 	
 	private Text mensageText;
+	private Text highscoreText;
+	private Text scoreText; 
 
 	@Override
 	public void populate() {
@@ -18,9 +21,13 @@ public class EndScene extends AbstractScene {
 		
 		mensageText = new Text(400, 350, res.font, "YOU PASSED!", new TextOptions(HorizontalAlign.CENTER), vbom);
 		mensageText.setScale(2.0f);
+		highscoreText = new Text(400, 200, res.font, "HIGH SCORE: " + activity.getHiScore(), new TextOptions(HorizontalAlign.CENTER), vbom);
+		scoreText = new Text(400, 100, res.font, "SCORE: " + GameManager.getInstance().getCurrentScore(), new TextOptions(HorizontalAlign.CENTER), vbom);
 		
 		menuScene.attachChild(mensageText);
-		
+		menuScene.attachChild(highscoreText);
+		menuScene.attachChild(scoreText);
+
 		menuScene.buildAnimations();
 		menuScene.setBackgroundEnabled(true);
 		setChildScene(menuScene);
