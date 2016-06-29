815bf633be11d02ca9c03bff6b5ff26697101ecc
Ivan Sena <ivansena35@gmail.com>
2016-06-29 01:31:08 -0300

    ajuste na transição de fases

--- LabCrisis/src/poo/trabalho/labcrisis/scene/FaseTutorial.java
+++ LabCrisis/src/poo/trabalho/labcrisis/scene/FaseTutorial.java
@@ -204,7 +204,7 @@ public class FaseTutorial extends AbstractScene {
 		ICollisionCallback myCollisionCallbackF = new ICollisionCallback() {		
 			@Override
 			public boolean onCollision(IShape pCheckShape, IShape pTargetShape) {
-				SceneManager.getInstance().showGameScene01();
+				SceneManager.getInstance().showGameScene00();
 				return false;
 			}
 		};
